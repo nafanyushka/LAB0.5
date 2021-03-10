46ab2e1e2bc7f07a5ef6f53868ff47eaa309bd95
Nafany <bausov.vadim@yandex.ru>
2021-03-11 01:24:17 +0300

    All right, but bug with 1 chet and 1 nechet nums

--- main_b.c
+++ main_b.c
@@ -9,50 +9,107 @@ struct Node{
     Node* next;
 };
 //                          STRUCTURES
+//                          FUNCS
 char* get_String();
 void print(Node*);
-//void factoring(Node* a, Node* chet, Node* nechet);
+void factoring(Node* a, Node* chet, Node* ne_chet);
 Node* strict(char*);
+//                          FUNCS
 
 int main(){
     Node * list = NULL;
-//    char* a = get_String();
-    printf("Your string: ");
-    list = strict(get_String());
-    printf("Only digits: ");
-    print(list);
+    Node * chet = (Node*)malloc(sizeof(Node));
+    Node * ne_chet = (Node*)malloc(sizeof(Node));
+    char* a = (char*)malloc(19);
+    while(*a != EOF) {
+        printf("Your string: ");
+        a = get_String();
+        if(*a == '\0') {
+            continue;
+            free(a);
+        }
+        list = strict(a);
+        free(a);
+        printf("Only digits: ");
+        print(list);
+        printf("\nChet and Ne_chet: \nChet: ");
+        factoring(list, chet, ne_chet);
+        print(chet);
+        printf("Ne_chet: ");
+        print(ne_chet);
+        printf("\n");
+    }
     system("pause");
     return 0;
 }
 
 void print(Node* first){
+    printf("\"");
     for(Node * p = first; p->next != NULL; p = p->next) {
         printf("%c", p->data);
     }
-    printf("\n");
+    printf("\"\n");
 }
 
-//void factoring(Node* a, Node* chet, Node* nechet){
-//    for(Node* p = a; p != NULL; p = p->next){
-//
-//    }
-//}
+void factoring(Node* a, Node* chet, Node* ne_chet){
+    Node* List_chet = chet;
+    Node* List_ne_chet = ne_chet;
+    char helper1 = ' ', helper = ' ';
+    for(Node* p = a; p->next != NULL; p = p->next){
+        if((((int)p->data)%2 == 1)){
+            List_ne_chet->data = p->data;
+            helper = List_ne_chet->data;
+            List_ne_chet->next = (Node*)malloc(sizeof(Node));
+            List_ne_chet = List_ne_chet->next;
+        }
+        if((((int)p->data)%2 == 0) && (48 <= (int)p->data && (int)p->data <= 57)) {
+            List_chet->data = p->data;
+            helper1 = List_chet->data;
+            List_chet->next = (Node*)malloc(sizeof(Node));
+            List_chet = List_chet->next;
+        }
+        if(p->data == ' ' && helper1 != ' ') {
+            List_chet->data = ' ';
+            helper1 = List_chet->data;
+            List_chet->next = (Node *) malloc(sizeof(Node));
+            List_chet = List_chet->next;
+        }
+        if(p->data == ' ' && helper != ' '){
+            List_ne_chet->data = ' ';
+            helper = List_ne_chet->data;
+            List_ne_chet->next = (Node*)malloc(sizeof(Node));
+            List_ne_chet = List_ne_chet->next;
+        }
+//        printf("%d", ((int)p->data)%2);
+    }
+    List_ne_chet->next = NULL;
+    List_chet->next = NULL;
+
+}
 
 Node* strict(char* a){
     Node* list = (Node*)malloc(sizeof(Node));
     Node* p = list;
-    int i = *a;
+    int i = (int)*a;
+    char helper;
     while(*a != '\0'){
-        if(48 <= i && i <= 57)
+        if((48 <= i && i <= 57) || *a == ' ')
 //        if(*a == '0'|| *a == '1' || *a == '2' || *a == '3' || *a == '4' || *a == '5' || *a == '6' || *a == '7' || *a == '8' || *a == '9')
         {
-            list->data = *a;
-            list->next = (Node *) malloc(sizeof(Node));
+//            printf("\nList data: %c; *a: %c", list -> data, *a);
+            if(helper == ' ' && *a == ' ') {
+
+            }
+            else{
+                list->data = *a;
+                helper = list->data;
+                list->next = (Node *) malloc(sizeof(Node));
 //        printf("%c\n", list->data);
-            list = list->next;
+                list = list->next;
+            }
         }
         a++;
-        i = *a;
+        i = (int)*a;
     }
     list->next = NULL;
 //    printf("END OF HYPE!\n");
