dd46b2ad0aec6df11e62f27bd19b72ccf21db5ae
Nafany <bausov.vadim@yandex.ru>
2021-03-10 13:35:33 +0300

    Restricting

--- main_b.c
+++ main_b.c
@@ -5,32 +5,52 @@
 //                          STRUCTURES
 typedef struct Node Node;
 struct Node{
-    char* data;
+    char data;
     Node* next;
 };
 //                          STRUCTURES
 char* get_String();
 void print(Node*);
+//void factoring(Node* a, Node* chet, Node* nechet);
+Node* strict(char*);
 
 int main(){
     Node * list = NULL;
-    Node a, b, c;
-    a.data = get_String();
-    b.data = get_String();
-    c.data = get_String();
-    a.next = &b;
-    b.next = &c;
-    c.next = NULL;
-    list = &a;
+    printf("Input your string: ");
+    char* a = get_String();
+    printf("Your string: ");
+    list = strict(a);
     print(list);
     system("pause");
     return 0;
 }
 
 void print(Node* first){
-    for(Node * p = first; p != NULL; p = p->next) {
-        printf("%s\n", p->data);
+    for(Node * p = first; p->next != NULL; p = p->next) {
+        printf("%c", p->data);
     }
+    printf("\n");
+}
+
+//void factoring(Node* a, Node* chet, Node* nechet){
+//    for(Node* p = a; p != NULL; p = p->next){
+//
+//    }
+//}
+
+Node* strict(char* a){
+    Node* list = (Node*)malloc(sizeof(Node));
+    Node* p = list;
+    while(*a != '\0'){
+        list->data = *a;
+        list->next = (Node*)malloc(sizeof(Node));
+//        printf("%c\n", list->data);
+        list = list->next;
+        a++;
+    }
+    list->next = NULL;
+//    printf("END OF HYPE!\n");
+    return p;
 }
 
 char* get_String(){
