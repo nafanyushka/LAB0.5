8e64cb5f79b9779b14a509ec8b24eed8c85102f0
Nafany <bausov.vadim@yandex.ru>
2021-03-18 19:00:49 +0300

    Ciplenok broylera (anti_space, memory free, hello)

--- main_b.c
+++ main_b.c
@@ -14,14 +14,15 @@ char* get_String();
 void print(Node*);
 void factoring(Node* a, Node* chet, Node* ne_chet);
 Node* strict(char*);
+void freestrct(Node*);
 //                          FUNCS
 
 int main(){
-    Node * list = NULL;
-    Node * chet = (Node*)malloc(sizeof(Node));
-    Node * ne_chet = (Node*)malloc(sizeof(Node));
     char* a = (char*)malloc(19);
     while(*a != EOF) {
+        Node * list = NULL;
+        Node * chet = (Node*)malloc(sizeof(Node));
+        Node * ne_chet = (Node*)malloc(sizeof(Node));
         printf("Your string: ");
         a = get_String();
         if(*a == '\0') {
@@ -38,6 +39,9 @@ int main(){
         printf("Ne_chet: ");
         print(ne_chet);
         printf("\n");
+        freestrct(list);
+        freestrct(chet);
+        freestrct(ne_chet);
     }
     system("pause");
     return 0;
@@ -54,41 +58,50 @@ void print(Node* first){
 void factoring(Node* a, Node* chet, Node* ne_chet){
     Node* List_chet = chet;
     Node* List_ne_chet = ne_chet;
-    Node* last_space;
+    Node* last_space = NULL;
     char helper1 = ' ', helper = ' ';
     for(Node* p = a; p->next != NULL; p = p->next){
-        if((((int)p->data)%2 == 1)){
+        if((((int)p->data)%2 == 1 && (48 <= (int)p->data && (int)p->data <= 57))){
             List_ne_chet->data = p->data;
             helper = List_ne_chet->data;
             List_ne_chet->next = (Node*)malloc(sizeof(Node));
+            if((p->next)->data == ' ')
+                last_space = List_ne_chet;
             List_ne_chet = List_ne_chet->next;
         }
         if((((int)p->data)%2 == 0) && (48 <= (int)p->data && (int)p->data <= 57)) {
             List_chet->data = p->data;
             helper1 = List_chet->data;
             List_chet->next = (Node*)malloc(sizeof(Node));
+            if((p->next)->data == ' ')
+                last_space = List_chet;
             List_chet = List_chet->next;
         }
         if(p->data == ' ' && helper1 != ' ') {
             List_chet->data = ' ';
             helper1 = List_chet->data;
-            last_space = List_chet;
+//            last_space = List_chet;
             List_chet->next = (Node *) malloc(sizeof(Node));
             List_chet = List_chet->next;
         }
         if(p->data == ' ' && helper != ' '){
             List_ne_chet->data = ' ';
             helper = List_ne_chet->data;
-            last_space = List_ne_chet;
+//            last_space = List_ne_chet;
             List_ne_chet->next = (Node*)malloc(sizeof(Node));
             List_ne_chet = List_ne_chet->next;
         }
-//        printf("%d", ((int)p->data)%2);
     }
     List_ne_chet->next = NULL;
     List_chet->next = NULL;
-    if(last_space->next->next == NULL)
-        last_space->next = NULL;
+    if(last_space != NULL) {
+    Node* anal = last_space->next;
+        if (anal->next->next == NULL) {
+            free(last_space->next);
+            last_space->next->next = NULL;
+        }
+    }
+
 
 }
 
@@ -99,9 +112,7 @@ Node* strict(char* a){
     char helper;
     while(*a != '\0'){
         if((48 <= i && i <= 57) || *a == ' ')
-//        if(*a == '0'|| *a == '1' || *a == '2' || *a == '3' || *a == '4' || *a == '5' || *a == '6' || *a == '7' || *a == '8' || *a == '9')
         {
-//            printf("\nList data: %c; *a: %c", list -> data, *a);
             if(helper == ' ' && *a == ' ') {
 
             }
@@ -109,7 +120,6 @@ Node* strict(char* a){
                 list->data = *a;
                 helper = list->data;
                 list->next = (Node *) malloc(sizeof(Node));
-//        printf("%c\n", list->data);
                 list = list->next;
             }
         }
@@ -117,13 +127,15 @@ Node* strict(char* a){
         i = (int)*a;
     }
     list->next = NULL;
-//    printf("END OF HYPE!\n");
     free(a);
     return p;
 }
 
 char* get_String(){
     char* a = (char*)malloc(1);
+    if(a == NULL){
+        printf("SOSNULL!!!!!!!!!!!!!!!!!!!!!!");
+    }
     char buf[11];
     int n, size = 0;
     *a = '\0';
@@ -142,5 +154,19 @@ char* get_String(){
             strcat(a, buf);
         }
     }while(n > 0);
+    if(*(a + strlen(a) - 1) == ' '){
+        for(int i = strlen(a) - 1; *(a + i) == ' '; i--)
+            *(a+i) = '\0';
+    }
     return a;
+}
+
+void freestrct(Node* a){
+    Node* helper = NULL;
+    while(a->next != NULL){
+        helper = a;
+        a = a->next;
+        if(helper != NULL)
+            free(helper);
+    }
 }
